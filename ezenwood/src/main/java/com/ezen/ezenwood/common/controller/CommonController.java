48894eb633a577c09b0d48b450db883daebcea4e
insu <k1m1nsu9805@gmail.com>
2022-05-27 17:51:59 +0900

    서블렛 컨텍스트 맵핑 수정 등등

---
+++ ezenwood/src/main/java/com/ezen/ezenwood/common/controller/CommonController.java
@@ -0,0 +1,30 @@
+package com.ezen.ezenwood.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/main")
+	public String main() {
+		return "main";
+	}
+	@RequestMapping("/about")
+	public String about() {
+		return "common/about";
+	}
+	@RequestMapping("/cart")
+	public String cart() {
+		
+		//모델 추가
+		return "common/cart";
+	}
+	
+
+}
