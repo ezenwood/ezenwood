cbf71eddcc0372fdf0e29d9948b109fa75bb1012
insu <k1m1nsu9805@gmail.com>
2022-05-27 17:57:59 +0900

    인덱스 제이에스피

--- ezenwood/src/main/java/com/ezen/ezenwood/common/controller/CommonController.java
+++ ezenwood/src/main/java/com/ezen/ezenwood/common/controller/CommonController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CommonController {
 	
-	@RequestMapping("/index")
-	public String index() {
-		return "index";
-	}
+//	@RequestMapping("/")
+//	public String index() {
+//		return "index";
+//	}
 	
 	@RequestMapping("/main")
 	public String main() {
