1a0a9ee2b9b6912588ed9035e4cda8128789b3cc
SaWonJin <sa6118@naver.com>
2022-06-10 18:10:06 +0900

    mypage final file
    마이페이지 관련 파일 마무리

--- ezenwood/src/main/java/com/ezen/ezenwood/mypage/service/MyPageServiceImpl.java
+++ ezenwood/src/main/java/com/ezen/ezenwood/mypage/service/MyPageServiceImpl.java
@@ -1,23 +1,36 @@
 package com.ezen.ezenwood.mypage.service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.print.DocFlavor.STRING;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
+import com.ezen.ezenwood.common.dao.SubImageDAO;
+import com.ezen.ezenwood.goods.dao.GoodsDAO;
+import com.ezen.ezenwood.goods.dao.GoodsDAOImpl;
 import com.ezen.ezenwood.mypage.dao.MyPageDAO;
+import com.ezen.ezenwood.mypage.dao.MyPageDAOImle;
 
 @Service("MyPageService")
-public class MyPageServiceImpl implements MyPageService{
+public class MyPageServiceImpl implements MyPageService {
 
 	Logger log = Logger.getLogger(this.getClass());
-	
+
 	@Resource(name = "MyPageDAO")
-	private MyPageDAO mypageDAO;
-	
+	MyPageDAOImle mypageDAO;
+
+	@Resource(name = "SubImageDAO")
+	SubImageDAO subImageDAO;
+
+	@Resource(name = "GoodsDAO")
+	GoodsDAOImpl goodsDAO;
 	
 	@Override
 	public Map<String, Object> memberInfo(Map<String, Object> map) throws Exception {
@@ -35,7 +48,7 @@ public class MyPageServiceImpl implements MyPageService{
 	}
 
 	@Override
-	public List<Map<String,Object>> orderList(Map<String, Object> map) throws Exception {
+	public List<Map<String, Object>> orderList(Map<String, Object> map) throws Exception {
 		return mypageDAO.orderList(map);
 	}
 
@@ -45,13 +58,35 @@ public class MyPageServiceImpl implements MyPageService{
 	}
 
 	@Override
-	public List<Map<String,Object>> cancelOrder(Map<String, Object> map) throws Exception {
+	public List<Map<String, Object>> cancelOrder(Map<String, Object> map) throws Exception {
 		return mypageDAO.cancelOrder(map);
 	}
 
 	@Override
-	public List<Map<String, Object>> memberReivewList(Map<String, Object> map) throws Exception {
-		return mypageDAO.memberReivewList(map);
+	public List<Map<String, Object>> memberReivewList(Map<String, Object> map){
+		List<Map<String,Object>> listMap = mypageDAO.memberReivewList(map);
+		
+		
+		for(Map<String,Object> goodsmap : listMap) {
+			Map<String, Object> amap = new HashMap<String, Object>();
+			
+			
+			String GOODS_TITLE = ((BigDecimal)goodsmap.get("REVIEW_PARENT")).toString();
+			int SUBIMAGE_PARENT = ((BigDecimal)goodsmap.get("REVIEW_PARENT")).intValue();
+			
+			amap.put("GOODS_NUM", goodsmap.get("REVIEW_PARENT"));
+			
+			Map<String, Object> bmap = goodsDAO.goodsDetail(amap);
+			
+			goodsmap.put("GOODS", bmap);
+			
+			
+			String GOODS_SUBIMAGE = subImageDAO.getSubImageByIDX(SUBIMAGE_PARENT);
+			String goods = goodsDAO.getGoodsTitle(GOODS_TITLE);
+			goodsmap.put("GOODS_TITLE", goods);
+			goodsmap.put("GOODS_SUBIMAGE", GOODS_SUBIMAGE);
+			}
+		return listMap;
 	}
 
 	@Override
